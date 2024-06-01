30b3ba54e7c55468ae0add0549258e1f94316e2b
Muhammad Hassan Bilal <hassansheikh9p9@gmail.com>
2024-06-01 19:36:24 +0500

    Fixed Deleting from Local Storage issue

--- list.js
+++ list.js
@@ -1,79 +1,69 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const input = document.querySelector('#userInput');
     const enter = document.querySelector('#enter');
     const ul = document.querySelector('ul');
-    
-    const display=()=>{
+
+    const display = () => {
         const tasks = getitems();
-        if(tasks.length>0){
+        if (tasks.length > 0) {
             tasks.forEach(task => {
-                createTask(task.id,task.value); 
+                createTask(task.id, task.value);
             });
         }
     }
 
-    addItem=e=>{
+    addItem = e => {
         e.preventDefault();
         const id = new Date().getTime().toString();
         const value = input.value;
-        if(value){
-            createTask(id,value);
+        if (value) {
+            createTask(id, value);
             input.value = "";
-            addLocal(id,value);
-        }else alert("Please enter something");
+            addLocal(id, value);
+        } else alert("Please enter something");
     }
-    
+
     window.addEventListener('DOMContentLoaded', display);
     enter.addEventListener('click', addItem);
 
-    delItem=e=>{
+    delItem = (id, e) => {
         const parent = e.currentTarget.parentElement;
         parent.remove();
         input.value = "";
-        const id = parent.dataset.id;
+        console.log(id);
         removeLocal(id);
-        if(ul.children.length == 0 ){
+        if (ul.children.length == 0) {
             localStorage.clear();
         }
     }
 
-    getitems=()=> localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+    const getitems = () => localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
 
 
-    addLocal=(id,value)=>{
-        const item = {id, value}; 
+    addLocal = (id, value) => {
+        const item = { id, value };
         const tasks = getitems();
         tasks.push(item);
         localStorage.setItem("list", JSON.stringify(tasks));
     }
 
-    removeLocal=id=>{
+    removeLocal = id => {
         const tasks = getitems();
-        for(i in tasks){
-            if(tasks[i].id=== id){
-                tasks.splice(i,1);
-            }
-        }
-        localStorage.setItem("list",JSON.stringify(tasks));
+        const fitleredTasks = tasks.filter((task => task.id !== id));
+        console.log(fitleredTasks);
+        localStorage.setItem("list", JSON.stringify(fitleredTasks));
     }
 
-    createTask=(id,value)=>{
+    createTask = (id, value) => {
         const list = document.createElement('li');
-            list.setAttribute("data-id", id);
-            list.innerHTML= `${value}<button class="delBtn">X</button>`;
-            const delBtn = list.querySelector('.delBtn');
-            delBtn.addEventListener('click', function(){
-                this.parentElement.remove();
-                const id = this.parentElement.id;
-                removeLocal(id);
-                if(ul.children.length == 0 ){
-                localStorage.clear();
-                }
-            });
-            ul.appendChild(list);
-            list.addEventListener('click', function(){
-                this.classList.toggle('done');
-            });
+        list.setAttribute("data-id", id);
+        list.innerHTML = `${value}<button class="delBtn">X</button>`;
+        const delBtn = list.querySelector('.delBtn');
+        delBtn.addEventListener('click', delItem.bind(null, id));
+        ul.appendChild(list);
+        list.addEventListener('click', function () {
+            this.classList.toggle('done');
+        });
     }
 
 });
